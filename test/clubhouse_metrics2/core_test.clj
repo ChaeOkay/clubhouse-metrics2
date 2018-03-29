7a091d249a625956a66904dd546ae03637fb4398
michaelsimonsonhf <michaels@healthfinch.com>
2018-03-29 12:30:16 -0500

    project shell

---
+++ test/clubhouse_metrics2/core_test.clj
@@ -0,0 +1,7 @@
+(ns clubhouse-metrics2.core-test
+  (:require [clojure.test :refer :all]
+            [clubhouse-metrics2.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
