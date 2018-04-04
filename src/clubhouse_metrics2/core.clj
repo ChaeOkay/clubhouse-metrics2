65cea36ab58e9019c9fecd8c53156699b1d47da3
michaelsimonsonhf <michaels@healthfinch.com>
2018-04-04 16:54:42 -0500

    updates to get stories and start parsing

--- src/clubhouse_metrics2/core.clj
+++ src/clubhouse_metrics2/core.clj
@@ -2,7 +2,8 @@
   (:require [clj-http.client :as client]
             [clojure.java.io :as io]
             [clojure.edn :as edn]
-            [clojure.data.json :as json])
+            [clojure.data.json :as json]
+            [clojure.string :as string])
            ; [cheshire.core :refer :all])
   (:gen-class))
 
@@ -10,12 +11,19 @@
 (defn -main
   "To be described"
   [& args]
-  ;;this will be a func
+  ;; validate clubhouse access
   (def access
    (with-open [r (io/reader ".secrets.edn")]
     (edn/read (java.io.PushbackReader. r))))
-  ;;this will be a parsing function
-  (def storycall (client/get (apply str "https://api.clubhouse.io/api/v2/stories/8022?token=" (access :token)) {:as :json}))
-  ;;this will be a calculation
-  ;;this will be an output function
+  ;;take in story
+  ;;make api call
+  (def storycall (client/get (apply str "https://api.clubhouse.io/api/v2/stories/6236?token=" (access :token)) {:as :json}))
+  ;;parse/save call output to file
+  (clojure.pprint/pprint storycall (clojure.java.io/writer "output.json"))
+  ;;perform story calculations, output to file
+  (def timer 
+    (with-open [r (io/reader "output.json")]
+      (edn/read (java.io.PushbackReader. r))))
+  (def starttime (get-in timer [:body :started_at]))
+  (def endtime (get-in timer [:body :completed_at]))
   (println "Hello, World!"))
